67e351056134edb9dd88ba3f4fdb4ec1278c3968
jacob679213 <47827860+jacob679213@users.noreply.github.com>
2021-03-24 14:13:40 -0400

    Added the create room modal
    Currently nonfunctional, but the HTML and CSS to make it work are all there

--- public/script.js
+++ public/script.js
@@ -55,3 +55,24 @@ function die(){
         
     }
 }
+
+
+
+//Modal stuffs
+
+//get modal div
+const modal = document.getElementById("createRoom")
+
+function openModal(){
+    modal.style.display = "block";
+}
+
+function closeModal(){
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
